9d9cdfedef60ee98d411e9c4c1f9404776574ac5
Leonardo Bichi <leonardobichi1@gmail.com>
2024-09-12 19:38:04 -0300

    Subida template base

---
+++ src/test/java/com/bichileonardo/plantapp/PlantappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bichileonardo.plantapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
